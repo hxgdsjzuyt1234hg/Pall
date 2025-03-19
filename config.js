011dd54c5fa3e82625bd6cc0c8368c7e6d9eeef4
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-19 21:18:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  BOT_TOKEN: "7585283331:AAFBTZMSJrBLhAvEYSTkS5OX93vFchNoHqw",
+  OWNER_ID: ["7865363008"],
+};
+
+/*/━━━━━━━━━━━━━━━━━━━━━━  
+🔥 XVINCENT 🔥  
+━━━━━━━━━━━━━━━━━━━━━━  
+
+🚀 Developer  : RenXiter  
+📢 Info       : @RenIsDev  
+🛠️ Version    : 5.0  
+
+⚠️ **警告!** ⚠️  
+📌 此机器人使用数据库。  
+📌 如果您的令牌 **未在数据库中注册**，  
+📌 机器人将 **无法运行**！  
+📌 请确保您的令牌已注册后再使用本机器人。  
+
+⚠️ **PERINGATAN!** ⚠️  
+📌 Bot ini menggunakan database.  
+📌 Jika token Anda **tidak terdaftar di database**,  
+📌 bot **tidak akan berjalan**!  
+📌 Pastikan token Anda sudah terdaftar sebelum menggunakan bot ini.  
+
+━━━━━━━━━━━━━━━━━━━━━━/*/
